6b2e27c7c1efc18f781d77f0334499b2cd5bf685
Ryan Pineo <ry@tsl.io>
2016-03-17 12:17:54 -0400

    fix bugs while attempting to deploy

---
+++ baseapp-auth/baseapp_auth/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext_lazy as _
+
+from .forms import UserCreationForm, UserChangeForm
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'date_joined', 'last_login')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_superuser')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    form = UserChangeForm
+    add_form = UserCreationForm
+    list_display = ('id', 'email', 'is_active', 'is_superuser',)
+    list_filter = ('is_superuser', 'is_active',)
+    search_fields = ('email',)
+    ordering = ('id',)
+    filter_horizontal = ()
