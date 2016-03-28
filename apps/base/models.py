7a5f13c26b51d198f3d5adb2e1c1704d5a3ce757
Ryan Pineo <ry@tsl.io>
2016-03-28 14:28:36 -0400

    add random_name_in helper for image uploads

--- apps/base/models.py
+++ apps/base/models.py
@@ -1,4 +1,8 @@
+import os
+import uuid
+
 from django.db import models
+from django.utils.deconstruct import deconstructible
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -11,3 +15,14 @@ class CaseInsensitiveTextField(models.TextField):
 
 class CaseInsensitiveEmailField(CaseInsensitiveTextField, models.EmailField):
     description = _("Case insensitive email address")
+
+
+@deconstructible
+class random_name_in(object):
+    def __init__(self, dir):
+        self.dir = dir
+
+    def __call__(self, instance, filename):
+        ext = filename.split('.')[-1]
+        filename = '{}.{}'.format(uuid.uuid4(), ext)
+        return os.path.join(self.dir, filename)
