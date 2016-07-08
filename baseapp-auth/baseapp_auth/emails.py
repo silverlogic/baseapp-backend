6bd863c16847bc3dd688e4f812a3f397be359d41
Brian Kim <bkim8815@gmail.com>
2016-07-08 14:24:41 -0400

    BA-149 add forgot password api

--- baseapp-auth/baseapp_auth/emails.py
+++ baseapp-auth/baseapp_auth/emails.py
@@ -8,3 +8,10 @@ def send_welcome_email(user):
     message = render_to_string('users/emails/welcome-body.txt.j2', context=context)
     html_message = render_to_string('users/emails/welcome-body.html.j2', context=context)
     send_mail(subject, message, html_message=html_message, from_email=None, recipient_list=[user.email])
+
+
+def send_password_reset_email(info):
+    subject = render_to_string('users/emails/password-reset-subject.txt.j2', info).strip()
+    message = render_to_string('users/emails/password-reset-body.txt.j2', info)
+    html_message = render_to_string('users/emails/password-reset-body.html.j2', info)
+    send_mail(subject, message, html_message=html_message, from_email=None, recipient_list=[info['email']])
---
+++ baseapp-auth/baseapp_auth/rest_framework/forgot_password/views.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.tokens import default_token_generator
+from django.core import signing
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+
+from rest_framework import mixins, response, status, viewsets
+
+from apps.users.emails import send_password_reset_email
+from apps.users.models import User
+
+from .serializers import ForgotPasswordSerializer, ResetPasswordSerializer
+
+
+class ForgotPasswordViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin):
+    serializer_class = ForgotPasswordSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        associated_user = User.objects.get(email=serializer.data['email'])
+        user_id = associated_user.pk
+        user_token = default_token_generator.make_token(associated_user)
+        token = urlsafe_base64_encode(force_bytes(signing.dumps([user_id, user_token])))
+        info = {'email': associated_user.email,
+                'token': token.decode('utf-8')}
+        send_password_reset_email(info)
+        return response.Response(ForgotPasswordSerializer(info).data, status=status.HTTP_200_OK)
+
+
+class ResetPasswordViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin):
+    serializer_class = ResetPasswordSerializer
+
+    def create(self, request, *arg, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return response.Response({})
