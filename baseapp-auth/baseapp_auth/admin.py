995f1ec0063f14f883d3f90f511d444435a9bb0f
Ryan Pineo <ry@tsl.io>
2016-07-25 16:06:12 -0400

    add first/last name to user

--- baseapp-auth/baseapp_auth/admin.py
+++ baseapp-auth/baseapp_auth/admin.py
@@ -9,8 +9,15 @@ from .models import User
 @admin.register(User)
 class UserAdmin(UserAdmin):
     fieldsets = (
-        (None, {'fields': ('email', 'password', 'date_joined', 'last_login')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_superuser')}),
+        (None, {
+            'fields': ('email', 'password', 'date_joined', 'last_login')
+        }),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_superuser')
+        }),
+        (_('Profile'), {
+            'fields': (('first_name', 'last_name'),)
+        }),
     )
     add_fieldsets = (
         (None, {
