5c78d98b1b7c5927868136d56ff349e80cb85e9e
Ryan Pineo <ry@tsl.io>
2016-08-04 13:01:47 -0400

    BA-244 fix social auth when email is not provided
    An access token can only be retrieved once for a given
    set of credentials.  This meant then when the second
    request containing the email came through the credentials
    were invalid.

---
+++ baseapp-auth/testproject/tests/integration/test_social_auth_cache_tasks.py
@@ -0,0 +1,12 @@
+import pytest
+
+from apps.social_auth_cache.models import SocialAuthAccessTokenCache
+from apps.social_auth_cache.tasks import clean_up_social_auth_cache
+
+pytestmark = pytest.mark.django_db
+
+
+class TestCleanUpSocialAuthCache:
+    def test_it_works(self):
+        SocialAuthAccessTokenCache.objects.create(access_token='asdb')
+        clean_up_social_auth_cache()
