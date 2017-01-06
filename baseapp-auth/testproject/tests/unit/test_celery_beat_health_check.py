10fab3faec257aa2a8c2417be82dfd7231895fcc
Ryan Pineo <ry@tsl.io>
2017-01-06 12:54:05 -0500

    add celery beat health check tests

---
+++ baseapp-auth/testproject/tests/unit/test_celery_beat_health_check.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.utils.module_loading import import_string
+
+import pytest
+
+
+@pytest.mark.parametrize('schedule', settings.CELERY_BEAT_SCHEDULE.values())
+def test_celery_beat_tasks_exist(schedule):
+    try:
+        import_string(schedule['task'])
+    except ImportError:
+        pytest.fail('celery beat task "{}" does not exist.'.format(schedule['task']))
