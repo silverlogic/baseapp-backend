1e10cc84cc88930fad3aa7cafb39822370db6191
Alisson Patricio <eu@alisson.net>
2021-10-26 18:54:18 +0000

    Merged in django-3-baseapp (pull request #105)
    Django 3.2 upgrade
    
    * update django 3 and other dependencies
    
    
    Approved-by: Octavio Iogha
    Approved-by: Filipe Oliveira
    Approved-by: Rafael Rocha

--- baseapp-auth/baseapp_auth/rest_framework/users/serializers.py
+++ baseapp-auth/baseapp_auth/rest_framework/users/serializers.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 
 from django.utils import timezone
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from avatar.models import Avatar
 from rest_framework import serializers
