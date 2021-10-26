4271bb3c287910681a337dbacfec2417583863ea
Alisson Patricio <eu@alisson.net>
2021-10-26 18:54:18 +0000

    Merged in django-3-baseapp (pull request #105)
    Django 3.2 upgrade
    
    * update django 3 and other dependencies
    
    
    Approved-by: Octavio Iogha
    Approved-by: Filipe Oliveira
    Approved-by: Rafael Rocha

--- apps/base/models.py
+++ apps/base/models.py
@@ -3,7 +3,7 @@ import uuid
 
 from django.db import models
 from django.utils.deconstruct import deconstructible
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class CaseInsensitiveTextField(models.TextField):
