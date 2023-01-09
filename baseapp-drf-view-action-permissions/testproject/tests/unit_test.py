7b7cf1188644db948ce1400b06555a7c23d4ee82
Sunday Nwuguru <sn@tsl.io>
2023-01-09 17:32:08 +0100

    Update tests and implement has_perms

--- baseapp-drf-view-action-permissions/testproject/tests/unit_test.py
+++ baseapp-drf-view-action-permissions/testproject/tests/unit_test.py
@@ -53,3 +53,18 @@ class TestUserPermissions:
 
         user = f.UserFactory(role=role)
         assert user.permission_list == set([f"testapp.{permission_2.codename}"])
+    
+    def test_user_has_perms(self):
+        permission_1 = Permission.objects.filter(codename='list_tests').first()
+        permission_2 = Permission.objects.filter(codename='test_disable').first()
+        permissions = [permission_1, permission_2]
+        role = f.RoleFactory(permissions=permissions)
+        user = f.UserFactory(role=role)
+
+        assert user.has_perm('testapp.list_tests')
+        assert user.has_perms(['testapp.list_tests', 'testapp.test_disable'])
+
+    def test_user_has_perms_override_for_super_user(self):
+        user = f.UserFactory(is_superuser=True)
+        assert user.has_perm('testapp.list_tests')
+        assert user.has_perms(['testapp.list_tests', 'testapp.test_disable'])
