c05ecf7545b079b93f7392a8e691a65b1980b975
Sunday Nwuguru <sn@tsl.io>
2023-01-09 18:31:58 +0100

    Fix migrations

--- baseapp-drf-view-action-permissions/testproject/testapp/migrations/0001_initial.py
+++ baseapp-drf-view-action-permissions/testproject/testapp/migrations/0001_initial.py
@@ -11,7 +11,6 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('auth', '0012_alter_user_first_name_max_length'),
-        ('permissions', '0002_alter_role_id'),
     ]
 
     operations = [
