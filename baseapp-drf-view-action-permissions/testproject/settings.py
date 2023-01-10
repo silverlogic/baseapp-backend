b606920cbad2e6df15523ea5bef207975c1227e9
Sunday Nwuguru <sn@tsl.io>
2023-01-10 18:43:03 +0100

    Remove bigauto field

--- baseapp-drf-view-action-permissions/testproject/settings.py
+++ baseapp-drf-view-action-permissions/testproject/settings.py
@@ -124,5 +124,3 @@ STATIC_ROOT = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
