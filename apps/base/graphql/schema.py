4314c03e1f4b6861fe264618412eff64c9d4ef3d
Alisson Patricio <eu@alisson.net>
2023-03-23 17:59:40 -0300

    CEO-180: add city to organization

--- apps/base/graphql/schema.py
+++ apps/base/graphql/schema.py
@@ -1,13 +1,14 @@
 import graphene
 from graphene import relay
 
+from apps.cities.graphql.queries import CitiesQuery
 from apps.comments.graphql.mutations import CommentsMutations
 from apps.comments.graphql.queries import CommentsQuery
 from apps.organizations.graphql.queries import ClassroomQuery
 from apps.users.graphql.queries import UsersQuery
 
 
-class Query(graphene.ObjectType, ClassroomQuery, CommentsQuery, UsersQuery):
+class Query(graphene.ObjectType, ClassroomQuery, CommentsQuery, UsersQuery, CitiesQuery):
     node = relay.node.NodeField(relay.Node)
 
 
