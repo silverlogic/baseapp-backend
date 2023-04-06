fd53606c907fef1fe2af96abaa9c48d33b5620d3
Gabriel Santos <gs@tsl.io>
2023-04-06 08:01:52 -0300

    CEO-297-be-reporting-posts-comments

--- apps/base/graphql/schema.py
+++ apps/base/graphql/schema.py
@@ -6,14 +6,18 @@ from apps.comments.graphql.mutations import CommentsMutations
 from apps.comments.graphql.queries import CommentsQuery
 from apps.organizations.graphql.queries import ClassroomQuery
 from apps.reactions.graphql.mutations import ReactionsMutations
+from apps.reports.graphql.mutations import ReportsMutations
+from apps.reports.graphql.queries import ReportsQuery
 from apps.users.graphql.queries import UsersQuery
 
 
-class Query(graphene.ObjectType, ClassroomQuery, CommentsQuery, UsersQuery, CitiesQuery):
+class Query(
+    graphene.ObjectType, ClassroomQuery, CommentsQuery, UsersQuery, CitiesQuery, ReportsQuery
+):
     node = relay.node.NodeField(relay.Node)
 
 
-class Mutation(graphene.ObjectType, CommentsMutations, ReactionsMutations):
+class Mutation(graphene.ObjectType, CommentsMutations, ReactionsMutations, ReportsMutations):
     pass
 
 
