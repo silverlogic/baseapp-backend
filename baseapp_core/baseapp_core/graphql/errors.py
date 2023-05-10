80582f935bab80c9534c7fc7b4fa23d7baa8e120
Alisson Patricio <eu@alisson.net>
2023-05-10 09:29:20 -0300

    add dependencies and remove graphql schema

--- baseapp_core/baseapp_core/graphql/errors.py
+++ baseapp_core/baseapp_core/graphql/errors.py
@@ -18,11 +18,3 @@ def LoginRequiredError():
         code="login_required",
         message=_("You should be logged in to perform this action"),
     )
-
-
-def InappropriateLanguageError():
-    return Error(
-        code="inappropriate_language",
-        location="content",
-        message=_("Please be advised that the use of inappropriate language is not permitted."),
-    )
