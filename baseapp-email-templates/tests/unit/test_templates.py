8f8472872b0214a9d0636d353983c96ca3493dbb
Julian Northover <jn@tsl.io>
2023-06-13 13:55:15 +0000

    Merged in BA-1055-api-create-e-mail-templates (pull request #1)
    BA-1055: DJ admin customized e-mails
    
    * update import
    
    * fix migration
    
    * fixes
    
    * setup.cfg fixes
    
    * lint
    
    * add tests
    
    * update tests
    
    * add filters
    
    * update README
    
    * clean README
    
    
    Approved-by: Matt Galvis
    Approved-by: Rafael Rocha
    Approved-by: Alisson Patricio

---
+++ baseapp-email-templates/tests/unit/test_templates.py
@@ -0,0 +1,71 @@
+from unittest.mock import call, patch
+
+from django.test import override_settings
+
+import pytest
+
+from baseapp_email_templates.custom_templates import get_full_copy_template
+from baseapp_email_templates.models import EmailTemplate
+from baseapp_email_templates.sendgrid import get_personalization
+
+import tests.factories as f
+
+pytestmark = pytest.mark.django_db
+
+
+class TestEmailTemplates:
+    @pytest.fixture
+    def data(self):
+        template_data = {
+            "name": "Test Template",
+            "subject": "This is a test",
+            "html_content": "<p>Hello</p>",
+            "sendgrid_template_id": "1234",
+        }
+        self.template = f.EmailTemplateFactory(**template_data)
+        return template_data
+
+    def test_auto_generate_plain_text(self, data):
+        html_content, text_content, subject = get_full_copy_template(self.template)
+
+        assert text_content == "Hello"
+
+    def test_send_mail(self, outbox, data):
+        self.template.send(["test@test.com"])
+
+        assert len(outbox) == 1
+
+        assert outbox[0].to == ["test@test.com"]
+        assert outbox[0].subject == data["subject"]
+        assert outbox[0].body == "Hello"
+        assert outbox[0].alternatives[0][0] == data["html_content"]
+
+    def test_send_mail_with_context(self, outbox, data):
+        self.template.html_content = "<p>{{content}}</p>"
+        context = {"content": "Test Content"}
+        self.template.send(["test@test.com"], context)
+
+        assert len(outbox) == 1
+
+        assert outbox[0].to == ["test@test.com"]
+        assert outbox[0].subject == data["subject"]
+        assert outbox[0].body == context["content"]
+        assert outbox[0].alternatives[0][0] == f"<p>{context['content']}</p>"
+
+    def test_get_personalization(self):
+        recipient = "test@test.com"
+        context = {"message": "Hello!"}
+        personalization = get_personalization(recipient, context)
+
+        assert personalization.dynamic_template_data == context
+        assert {'email': recipient} in personalization.tos
+
+    def test_send_via_sendgrid(self, outbox, data):
+        recipient = "test@test.com"
+        context = {"message": "Hello!"}
+        personalization = get_personalization(recipient, context)
+
+        self.template.send_via_sendgrid(personalization)
+
+        assert len(outbox) == 1
+
