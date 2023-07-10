8997f780f95ceb09a214e28e856e59486cd45784
Janusz Kacalak <jkacalak@gmail.com>
2023-07-10 08:08:06 -0400

    setup auth endpoints for JWT, Simple token and MFA

--- baseapp-auth/baseapp_auth/rest_framework/jwt/serializers.py
+++ baseapp-auth/baseapp_auth/rest_framework/jwt/serializers.py
@@ -1,13 +1,10 @@
-from django.contrib.auth import authenticate, get_user_model
+from baseapp_auth.rest_framework.login.serializers import LoginPasswordExpirationMixin
 from django.utils.translation import gettext_lazy as _
 
-from rest_framework import exceptions, serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
-from baseapp_auth.rest_framework.login.serializers import LoginSerializer
 
-
-class BaseJwtLoginSerializer(TokenObtainPairSerializer):
+class BaseJwtLoginSerializer(LoginPasswordExpirationMixin, TokenObtainPairSerializer):
     claim_serializer_class = None
 
     @classmethod
@@ -28,26 +25,7 @@ class BaseJwtLoginSerializer(TokenObtainPairSerializer):
 
         return token
 
-    def validate_email(self, email):
-        User = get_user_model()
-        try:
-            self.user = User.objects.get(email=email)
-        except User.DoesNotExist:
-            raise exceptions.ValidationError(_("Email does not exist."))
-        return email
-
     def validate(self, data):
-        self.validate_email(data["email"])
-        if not self.user.check_password(data["password"]):
-            raise exceptions.ValidationError({"password": _("Incorrect password.")})
-        if self.user.is_password_expired:
-            raise exceptions.AuthenticationFailed(
-                {
-                    "password": [
-                        _(
-                            "Your password has expired. Please reset it or contact the administrator."
-                        )
-                    ]
-                }
-            )
-        return super().validate(data)
+        validated_data = super().validate(data)
+        self.check_password_expiration(self.user)
+        return validated_data
--- baseapp-auth/testproject/tests/integration/api_v1/test_login.py
+++ baseapp-auth/testproject/tests/integration/api_v1/test_login.py
@@ -1,3 +1,4 @@
+import json
 from django.utils import timezone
 
 import pytest
@@ -35,22 +36,14 @@ class TestLogin(ApiMixin):
     def test_when_email_doesnt_exist(self, client, auth_method):
         data = {"email": "john@doe.com", "password": "1234567890"}
         r = self.send_login_request(client, auth_method, data)
-        h.responseBadRequest(r)
-        if auth_method == "simple_token":
-            assert r.data["email"] == ["Email does not exist."]
-        elif auth_method == "jwt":
-            assert r.data == {"error": "Unable to login with provided credentials."}
+        h.responseUnauthorized(r)
 
     @pytest.mark.parametrize("auth_method", ["simple_token", "jwt"])
     def test_when_password_doesnt_match(self, client, auth_method):
         data = {"email": "john@doe.com", "password": "1234567890"}
         f.UserFactory(email=data["email"], password="not password")
         r = self.send_login_request(client, auth_method, data)
-        h.responseBadRequest(r)
-        if auth_method == "simple_token":
-            assert r.data["password"] == ["Incorrect password."]
-        elif auth_method == "jwt":
-            assert r.data == {"error": "Unable to login with provided credentials."}
+        h.responseUnauthorized(r)
 
     @pytest.mark.parametrize("auth_method", ["simple_token", "jwt"])
     @override_config(USER_PASSWORD_EXPIRATION_INTERVAL=1)
@@ -60,10 +53,9 @@ class TestLogin(ApiMixin):
         r = self.send_login_request(client, auth_method, data)
         h.responseOk(r)
 
-    @pytest.mark.parametrize("auth_method", ["simple_token"])
+    @pytest.mark.parametrize("auth_method", ["simple_token", "jwt"])
     @override_config(USER_PASSWORD_EXPIRATION_INTERVAL=1)
     def test_cant_login_with_expired_password(self, client, auth_method):
-        # TODO: enable expired password logic for JWT
         data = {"email": "john@doe.com", "password": "1234567890"}
         user = f.UserFactory(email=data["email"], password=data["password"])
         user.password_changed_date = timezone.now() - timezone.timedelta(days=1)
@@ -75,7 +67,7 @@ class TestLogin(ApiMixin):
 
 class TestMfaLogin(ApiMixin):
     """
-    More detailed MFA tests are in the trench package.
+    More detailed MFA tests can be found in the "trench" package.
     """
 
     view_name = "login-list"
@@ -84,7 +76,7 @@ class TestMfaLogin(ApiMixin):
         if auth_method == "simple_token":
             return client.post(self.reverse(), data)
         elif auth_method == "jwt":
-            return client.post("/v1/auth/jwt/login/", data)
+            return client.post("/v1/auth/mfa/jwt/login/", data)
 
     @pytest.mark.parametrize("auth_method", ["simple_token", "jwt"])
     def test_can_login_using_mfa(self, client, active_user_with_application_otp, auth_method):
