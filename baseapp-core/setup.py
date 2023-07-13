67f32cad164d090f553abf9e9cc7d2fb318428a0
Daniel <do@tsl.io>
2023-07-13 14:48:01 -0300

    test release

--- baseapp-core/setup.py
+++ baseapp-core/setup.py
@@ -1,3 +1,12 @@
 from setuptools import setup
 
-setup()
+setup(
+    name="baseapp-core",  # How you named your package folder (MyLib)
+    packages=["baseapp-core"],  # Chose the same as "name"
+    version="0.6",  # Start with a small number and increase it with every change you make
+
+    install_requires=[  # I get to this in a second
+        "validators",
+        "beautifulsoup4",
+    ],
+)
--- baseapp-reactions/setup.py
+++ baseapp-reactions/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="baseapp-reactions",  # How you named your package folder (MyLib)
     packages=["baseapp-reactions"],  # Chose the same as "name"
-    version="0.6",  # Start with a small number and increase it with every change you make
+    version="0.7",  # Start with a small number and increase it with every change you make
 
     install_requires=[  # I get to this in a second
         "validators",
