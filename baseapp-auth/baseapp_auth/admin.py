2f54cd9d3b17598b92564fe9a4ac79e7a08e7813
Janusz Kacalak <jkacalak@gmail.com>
2023-07-14 11:51:55 -0400

    clean up baseapp-auth requirements

--- baseapp-auth/baseapp_auth/admin.py
+++ baseapp-auth/baseapp_auth/admin.py
@@ -4,7 +4,6 @@ from django.contrib.auth.admin import UserAdmin
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
-import swapper
 from constance import config
 
 from .emails import (
