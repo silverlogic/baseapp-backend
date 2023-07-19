fec549c90e5a45c8340f91835ebb22dc5f73e530
Daniel <do@tsl.io>
2023-07-19 14:09:56 -0300

    BA-1084: Add setup

--- baseapp-core/setup.py
+++ baseapp-core/setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
 
-if __name__ == "__main__":
-    setup(
-        name="baseapp-core",
-        packages=["baseapp-core"],
-        version="0.1",
-    )
+setup()
+# if __name__ == "__main__":
+#     setup(
+#         name="baseapp-core",
+#         packages=["baseapp-core"],
+#         version="0.1",
+#     )
