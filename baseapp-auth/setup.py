53555c2d84bc87787146054322770b91a5173aae
Daniel <do@tsl.io>
2023-07-19 14:57:43 -0300

    BA-1084: Add setup

--- baseapp-auth/setup.py
+++ baseapp-auth/setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup
 
-if __name__ == "__main__":
-    setup(
-        name="baseapp-auth",
-        packages=["baseapp-auth"],
-        version="0.1",
-    )
+
+setup(
+    name="baseapp-auth",
+    version="0.1",
+)
