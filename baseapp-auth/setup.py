cb13c622fe610a66f4882224c509cc0b624357c0
Daniel <do@tsl.io>
2023-07-19 17:27:47 -0300

    BA-1084: Fix

--- baseapp-auth/setup.py
+++ baseapp-auth/setup.py
@@ -1,7 +1,12 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name="baseapp-auth",
     packages=find_packages(),
     version="0.1",
+    long_description=long_description,
+    long_description_content_type="text/markdown"
 )
