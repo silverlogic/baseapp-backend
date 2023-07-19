697b77db3dee0f9bbcaa7b2d0dbdd5baa807e8f0
Daniel <do@tsl.io>
2023-07-19 15:53:24 -0300

    BA-1084: Fix

--- baseapp-auth/setup.py
+++ baseapp-auth/setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="baseapp-auth",
-    packages=["baseapp_auth"],
+    packages=find_packages(),
     version="0.2",
 )
