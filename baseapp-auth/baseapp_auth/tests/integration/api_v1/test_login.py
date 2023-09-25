358f4f5dedb362bbdabbc943216927e6086649b4
Gadhi <gr2@tsl.io>
2023-09-25 13:23:43 -0300

    Remove test project trailing slashes

--- baseapp-auth/baseapp_auth/tests/integration/api_v1/test_login.py
+++ baseapp-auth/baseapp_auth/tests/integration/api_v1/test_login.py
@@ -92,7 +92,7 @@ class TestLoginAuthToken(TestLoginBase):
 
 
 class TestLoginJwt(TestLoginBase):
-    login_endpoint_path = "/v1/auth/jwt/"
+    login_endpoint_path = "/v1/auth/jwt"
 
     @pytest.fixture
     def data(self):
