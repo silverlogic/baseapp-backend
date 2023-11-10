f990197395f552b58baa91f2cd38c96be5b270da
Gadhi <gr2@tsl.io>
2023-11-10 12:24:22 -0300

    BA-1220: Baseapp-core updates for tests modularization

--- baseapp-core/baseapp_core/tests/mixins.py
+++ baseapp-core/baseapp_core/tests/mixins.py
@@ -1,24 +1,21 @@
 import urllib.parse
 
-import pytest
 from django.urls import reverse
 
 
 class ApiMixin:
     view_name = ""
     url_kwargs = None
+    api_version = "v1"
 
     def reverse(self, view_name=None, query_params=None, **kwargs):
-        # Specified in integration/vX/conftest.py
-        version = pytest.api_version
-
         if self.url_kwargs is not None:
             kwargs.setdefault("kwargs", self.url_kwargs)
 
         if not view_name:
             view_name = self.view_name
 
-        url = reverse("{}:{}".format(version, view_name), **kwargs)
+        url = reverse("{}:{}".format(self.api_version, view_name), **kwargs)
 
         if query_params:
             url += "?" + urllib.parse.urlencode(query_params, doseq=True)
