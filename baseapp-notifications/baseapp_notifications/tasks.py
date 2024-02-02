b63328d696a4d307ebc72cd352ec8abd625037ec
vitorguima <vitorguimaraes@Vitors-MacBook-Pro.local>
2024-02-02 16:34:48 -0300

    fix: trigger push notification only if device exists

--- baseapp-notifications/baseapp_notifications/tasks.py
+++ baseapp-notifications/baseapp_notifications/tasks.py
@@ -13,26 +13,32 @@ def send_push_notification(
         WNSDevice,
     )
 
+    if not push_description:
+        raise Exception("push_description is required")
+
     # send messages to all Apple devices
     apple_devices = APNSDevice.objects.filter(user__id=user_id)
-    if apple_devices and push_title and push_description:
-        message = {"title": force_str(push_title), "body": force_str(push_description)}
     if apple_devices:
         message = force_str(push_description)
-    apple_devices.send_message(message=message, extra=extra)
+        if push_title and push_description:
+            message = {"title": force_str(push_title), "body": force_str(push_description)}
+        apple_devices.send_message(message=message, extra=extra)
 
     # send messages to all Android devices
     android_devices = GCMDevice.objects.filter(user__id=user_id)
-    if android_devices and push_description:
+    if android_devices:
         message = force_str(push_description)
-        if push_title:
-            extra.update({"title": force_str(push_title)})
-    android_devices.send_message(message=message, extra=extra)
+        if push_title and push_description:
+            android_devices.send_message(message, title=push_title, extra=extra)
+        else:
+            android_devices.send_message(message=message, extra=extra)
 
     # TO DO: send messages to all Windows devices
     windows_devices = WNSDevice.objects.filter(user__id=user_id)
-    windows_devices.send_message(message=message, extra=extra)
+    if windows_devices:
+        windows_devices.send_message(message=message, extra=extra)
 
     # TO DO: send messages to all Web devices
     web_devices = WebPushDevice.objects.filter(user__id=user_id)
-    web_devices.send_message(message=message, extra=extra)
+    if web_devices:
+        web_devices.send_message(message=message, extra=extra)
