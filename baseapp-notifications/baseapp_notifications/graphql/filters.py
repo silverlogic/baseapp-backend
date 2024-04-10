be9b5484137bd5a57ded7036c3397d2a6492f137
LP <lp@tsl.io>
2024-04-10 10:39:18 -0300

    [Baseapp Notifications] add verbs to filters

--- baseapp-notifications/baseapp_notifications/graphql/filters.py
+++ baseapp-notifications/baseapp_notifications/graphql/filters.py
@@ -5,6 +5,12 @@ Notification = swapper.load_model("notifications", "Notification")
 
 
 class NotificationFilter(django_filters.FilterSet):
+    verbs = django_filters.CharFilter(method="filter_by_verbs")
+
     class Meta:
         model = Notification
         fields = ["level", "unread"]
+
+    def filter_by_verbs(self, queryset, name, value):
+        verbs = value.split(",")
+        return queryset.filter(verb__in=verbs)
