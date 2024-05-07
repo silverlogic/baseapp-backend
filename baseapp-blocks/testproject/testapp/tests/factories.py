1b3cc86ba3e59bceda790cff17934a9fdf9eaf25
Alisson Patricio <eu@alisson.net>
2024-05-07 00:22:36 -0300

    baseapp-blocks: initial package

---
+++ baseapp-blocks/testproject/testapp/tests/factories.py
@@ -0,0 +1,11 @@
+import factory
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    email = factory.Faker("email")
+    password = factory.PostGenerationMethodCall("set_password", "default")
+    first_name = factory.Faker("first_name")
+    last_name = factory.Faker("last_name")
+
+    class Meta:
+        model = "testapp.User"
