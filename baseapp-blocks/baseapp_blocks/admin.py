4b4a04f30f286bab6d7ab3d7e914eb276195626c
Alisson Patricio <eu@alisson.net>
2024-05-07 00:22:36 -0300

    baseapp-blocks: initial package

---
+++ baseapp-blocks/baseapp_blocks/admin.py
@@ -0,0 +1,10 @@
+import swapper
+from django.contrib import admin
+
+Block = swapper.load_model("baseapp_blocks", "Block")
+
+
+@admin.register(Block)
+class BlockAdmin(admin.ModelAdmin):
+    list_display = ("id", "actor", "target", "created")
+    list_filter = ("created",)
