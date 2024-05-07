0f1cd5e49f4b778cdd0ac2c73ad2b2bcbb3bece7
Gadhi <gr2@tsl.io>
2024-05-07 10:22:01 -0600

    BA-1366: Use original PGHistory and add ASGI support

--- baseapp-core/baseapp_core/middleware.py
+++ baseapp-core/baseapp_core/middleware.py
@@ -2,9 +2,13 @@ import threading
 import uuid
 import zoneinfo
 
+import pghistory
 from django.conf import settings
+from django.core.handlers.asgi import ASGIRequest as DjangoASGIRequest
+from django.core.handlers.wsgi import WSGIRequest as DjangoWSGIRequest
 from django.utils import timezone
 from django.utils.deprecation import MiddlewareMixin
+from pghistory import config
 
 threading_local = threading.local()
 
@@ -30,3 +34,50 @@ class ThreadAttachedRequestLocalMiddleware(MiddlewareMixin):
         response = self.get_response(request)
         setattr(threading_local, "request_trace", None)
         return response
+
+
+class DjangoRequest:
+    """
+    Although Django's auth middleware sets the user in middleware,
+    apps like django-rest-framework set the user in the view layer.
+    This creates issues for pghistory tracking since the context needs
+    to be set before DB operations happen.
+
+    This special WSGIRequest updates pghistory context when
+    the request.user attribute is updated.
+    """
+
+    def __setattr__(self, attr, value):
+        if attr == "user":
+            pghistory.context(user=value.pk if value and hasattr(value, "pk") else None)
+
+        return super().__setattr__(attr, value)
+
+
+class WSGIRequest(DjangoRequest, DjangoWSGIRequest):
+    pass
+
+
+class ASGIRequest(DjangoRequest, DjangoASGIRequest):
+    pass
+
+
+def HistoryMiddleware(get_response):
+    def middleware(request):
+        if request.method in config.middleware_methods():
+            user = (
+                request.user.pk
+                if hasattr(request, "user") and hasattr(request.user, "pk")
+                else None
+            )
+            with pghistory.context(user=user, url=request.path):
+                if isinstance(request, DjangoWSGIRequest):  # pragma: no branch
+                    request.__class__ = WSGIRequest
+                elif isinstance(request, DjangoASGIRequest):  # pragma: no branch
+                    request.__class__ = ASGIRequest
+
+                return get_response(request)
+        else:
+            return get_response(request)
+
+    return middleware
