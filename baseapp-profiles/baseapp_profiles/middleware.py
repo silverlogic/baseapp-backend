f88de55415ee58b0d16076facb3505300422b16d
Filipe Oliveira <fc@tsl.io>
2024-06-17 20:14:04 +0200

    Add middleware error if

--- baseapp-profiles/baseapp_profiles/middleware.py
+++ baseapp-profiles/baseapp_profiles/middleware.py
@@ -8,12 +8,15 @@ Profile = swapper.load_model("baseapp_profiles", "Profile")
 class CurrentProfileMiddleware(BaseMiddleware):
     def process_request(self, request):
         current_profile_header = request.headers.get('Current-Profile')
-        if request.user.is_authenticated:
-            if current_profile_header:
-                try:
-                    pk = get_pk_from_relay_id(current_profile_header)
-                    request.user.current_profile = Profile.objects.get(pk=pk)
-                except Profile.DoesNotExist:
-                    request.user.current_profile = request.user.profile
+
+        if not request.user.is_authenticated:
+            return
+
+        if current_profile_header:
+            pk = get_pk_from_relay_id(current_profile_header)
+            profile = Profile.objects.first(pk=pk)
+
+            if profile and request.user.has_perm("baseapp_profiles.use_profile", profile):
+                request.user.current_profile = profile
             else:
                 request.user.current_profile = request.user.profile
