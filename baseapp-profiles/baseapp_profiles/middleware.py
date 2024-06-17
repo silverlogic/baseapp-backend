47950eb6fcb3192a0467fa0a42e93780a22a9a9b
Filipe Oliveira <fc@tsl.io>
2024-06-17 20:03:31 +0200

    Add middleware error except

--- baseapp-profiles/baseapp_profiles/middleware.py
+++ baseapp-profiles/baseapp_profiles/middleware.py
@@ -8,11 +8,15 @@ Profile = swapper.load_model("baseapp_profiles", "Profile")
 class CurrentProfileMiddleware(BaseMiddleware):
     def process_request(self, request):
         current_profile_header = request.headers.get('Current-Profile')
-        if current_profile_header:
-            try:
-                pk = get_pk_from_relay_id(current_profile_header)
-                request.user.current_profile = Profile.objects.get(pk=pk)
-            except Profile.DoesNotExist:
+
+        try: 
+            if current_profile_header:
+                try:
+                    pk = get_pk_from_relay_id(current_profile_header)
+                    request.user.current_profile = Profile.objects.get(pk=pk)
+                except Profile.DoesNotExist:
+                    request.user.current_profile = request.user.profile
+            else:
                 request.user.current_profile = request.user.profile
-        else:
-            request.user.current_profile = request.user.profile
+        except AttributeError:
+            pass
