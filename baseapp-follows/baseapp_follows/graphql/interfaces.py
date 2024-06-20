fc517bfd533275fcc2aaf0ee8ca823f68fb41e6a
Alisson Patricio <eu@alisson.net>
2024-06-20 00:29:32 -0300

    baseapp-follows: migrate to use baseapp-profiles

---
+++ baseapp-follows/baseapp_follows/graphql/interfaces.py
@@ -0,0 +1,45 @@
+import graphene
+import swapper
+from baseapp_core.graphql import get_object_type_for_model, get_pk_from_relay_id
+from django.contrib.contenttypes.models import ContentType
+from graphene import relay
+from graphene_django.filter import DjangoFilterConnectionField
+
+Follow = swapper.load_model("baseapp_follows", "Follow")
+Profile = swapper.load_model("baseapp_profiles", "Profile")
+
+
+class FollowsInterface(relay.Node):
+    followers = DjangoFilterConnectionField(get_object_type_for_model(Follow))
+    following = DjangoFilterConnectionField(get_object_type_for_model(Follow))
+    followers_count = graphene.Int()
+    following_count = graphene.Int()
+    is_followed_by_me = graphene.Boolean(
+        profile_id=graphene.ID(required=True),
+    )
+
+    def resolve_followers_count(self, info):
+        return self.followers_count
+
+    def resolve_following_count(self, info):
+        return self.following_count
+
+    def resolve_followers(self, info, **kwargs):
+        return self.followers.all()
+
+    def resolve_following(self, info, **kwargs):
+        return self.following.all()
+
+    def resolve_is_followed_by_me(self, info, profile_id, **kwargs):
+        if not info.context.user.is_authenticated:
+            return False
+        pk = get_pk_from_relay_id(profile_id)
+        profile = Profile.objects.get_if_member(pk=pk, user=info.context.user)
+        return (
+            profile
+            and Follow.objects.filter(
+                actor_id=profile.id,
+                target_content_type=ContentType.objects.get_for_model(self),
+                target_id=self.id,
+            ).exists()
+        )
