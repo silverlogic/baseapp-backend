1f2878dcb215d59829ce55ea5862c09ed34557b9
Alisson Patricio <eu@alisson.net>
2024-06-20 00:29:12 -0300

    baseapp-profiles: initial package

---
+++ baseapp-profiles/baseapp_profiles/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class BaseAppProfilesConfig(AppConfig):
+    name = "baseapp_profiles"
+    verbose_name = "BaseApp Profiles"
+    default_auto_field = "django.db.models.AutoField"
