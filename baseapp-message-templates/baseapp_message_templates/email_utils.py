d286d2043bdf1ccc14048dd4beecec808348d309
Pedro Tibúrcio <pt@tsl.io>
2024-08-21 08:30:55 -0400

    chore: PR suggestions

--- baseapp-message-templates/baseapp_message_templates/email_utils.py
+++ baseapp-message-templates/baseapp_message_templates/email_utils.py
@@ -1,28 +1,24 @@
 import logging
 
 from .models import EmailTemplate
+from .sendgrid import get_personalization
 
 logger = logging.getLogger(__name__)
 
 
-def send_template_email(
-    template_name=None,
-    recipient_list=[],
-    context={},
-    use_base_template=True,
-    extended_with="",
-    attachments=[],
-):
+def send_template_email(template_name, *args, **kwargs):
 
     try:
         template = EmailTemplate.objects.get(name=template_name)
+        template.send(*args, **kwargs)
+    except EmailTemplate.DoesNotExist:
+        logger.info(f'Email template "{template_name}" not found.')
 
-        template.send(
-            recipients=recipient_list,
-            context=context,
-            use_base_template=use_base_template,
-            extended_with=extended_with,
-            attachments=attachments,
-        )
+
+def send_sendgrid_email(template_name, recipient_email: str, context={}):
+    try:
+        template = EmailTemplate.objects.get(name=template_name)
+        personalization = get_personalization(recipient_email, context)
+        template.send_via_sendgrid(personalization)
     except EmailTemplate.DoesNotExist:
         logger.info(f'Email template "{template_name}" not found.')
