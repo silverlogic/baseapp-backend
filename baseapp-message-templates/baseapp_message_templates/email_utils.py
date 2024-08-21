9bc0f0fd36c7b6ced33bd5d56616fddcb85835b0
Pedro Tibúrcio <104370112+pt-tsl@users.noreply.github.com>
2024-08-21 14:53:57 -0400

    baseapp-message-templates: add send_template_email util (#138)
    * feat: add send_template_email to baseapp-message-template
    
    * chore: PR suggestions
    
    * feat: add sendgrid util
    
    * chore: update README and tests

---
+++ baseapp-message-templates/baseapp_message_templates/email_utils.py
@@ -0,0 +1,34 @@
+import logging
+
+from .models import EmailTemplate
+from .sendgrid import get_personalization
+
+logger = logging.getLogger(__name__)
+
+
+def send_template_email(template_name, *args, **kwargs):
+
+    try:
+        template = EmailTemplate.objects.get(name=template_name)
+        template.send(*args, **kwargs)
+    except EmailTemplate.DoesNotExist:
+        logger.error(f'Email template "{template_name}" not found.')
+
+
+def send_sendgrid_email(template_name, content=[]):
+    try:
+        template = EmailTemplate.objects.get(name=template_name)
+        if len(content) == 0:
+            raise ValueError("No personalizations provided.")
+        elif len(content) == 1:
+            message = content[0]
+            recipient, context = message
+            personalization = get_personalization(recipient, {"message": context})
+            template.send_via_sendgrid(personalization)
+        else:
+            personalizations = [
+                get_personalization(message[0], {"message": message[1]}) for message in content
+            ]
+            template.mass_send_via_sendgrid(personalizations)
+    except EmailTemplate.DoesNotExist:
+        logger.error(f'Email template "{template_name}" not found.')
