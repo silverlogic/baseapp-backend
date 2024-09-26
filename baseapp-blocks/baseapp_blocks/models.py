0738731a29ba152fe8ef4c9ecbc92fe58cee07cb
Alisson Patricio <eu@alisson.net>
2024-09-25 21:06:48 -0300

    baseapp-blocks: remove unused SwappableBlock

--- baseapp-blocks/baseapp_blocks/models.py
+++ baseapp-blocks/baseapp_blocks/models.py
@@ -14,9 +14,6 @@ class Block(AbstractBaseBlock):
         swappable = swapper.swappable_setting("baseapp_blocks", "Block")
 
 
-SwappableBlock = swapper.load_model("baseapp_blocks", "Block", required=False, require_ready=False)
-
-
 class BlockableModel(models.Model):
     blockers_count = models.PositiveIntegerField(default=0, editable=False)
     blocking_count = models.PositiveIntegerField(default=0, editable=False)
