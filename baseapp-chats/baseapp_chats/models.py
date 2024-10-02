f8af2eb0821ad0742515e1e109d8077bcc072f63
Alisson Patricio <eu@alisson.net>
2024-10-02 20:48:35 -0300

    baseapp-chats: fix migrations and object types customization

--- baseapp-chats/baseapp_chats/models.py
+++ baseapp-chats/baseapp_chats/models.py
@@ -34,17 +34,25 @@ class UnreadMessageCount(AbstractUnreadMessageCount):
 class Message(AbstractBaseMessage):
     class Meta(AbstractBaseMessage.Meta):
         swappable = swapper.swappable_setting("baseapp_chats", "Message")
-        triggers = [
-            set_last_message_on_insert_trigger(ChatRoom),
-            create_message_status_trigger(ChatRoomParticipant),
-            update_last_message_on_delete_trigger(ChatRoom),
-        ]
+        triggers = (
+            []
+            if swapper.is_swapped("baseapp_chats", "Message")
+            else [
+                set_last_message_on_insert_trigger(ChatRoom),
+                create_message_status_trigger(ChatRoomParticipant),
+                update_last_message_on_delete_trigger(ChatRoom),
+            ]
+        )
 
 
 class MessageStatus(AbstractMessageStatus):
     class Meta(AbstractMessageStatus.Meta):
         swappable = swapper.swappable_setting("baseapp_chats", "MessageStatus")
-        triggers = [
-            increment_unread_count_trigger(UnreadMessageCount, Message),
-            decrement_unread_count_trigger(UnreadMessageCount, Message),
-        ]
+        triggers = (
+            []
+            if swapper.is_swapped("baseapp_chats", "MessageStatus")
+            else [
+                increment_unread_count_trigger(UnreadMessageCount, Message),
+                decrement_unread_count_trigger(UnreadMessageCount, Message),
+            ]
+        )
