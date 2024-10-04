79a01cf8b4fbd52a6f3fdf84053ca039ce253525
Alisson Patricio <eu@alisson.net>
2024-10-04 10:09:56 -0300

    baseapp-core: wip activity log

--- baseapp-core/baseapp_core/graphql/views.py
+++ baseapp-core/baseapp_core/graphql/views.py
@@ -3,6 +3,7 @@ from graphene_django.views import GraphQLView as GrapheneGraphQLView
 from graphene_django.views import HttpError
 from graphql import get_operation_ast, parse
 from graphql.execution import ExecutionResult
+import pghistory
 
 try:
     import sentry_sdk
@@ -19,21 +20,22 @@ class GraphQLView(GrapheneGraphQLView):
                 return None
             raise HttpError(HttpResponseBadRequest("Must provide query string."))
 
+        try:
+            document = parse(query)
+        except Exception as e:
+            return ExecutionResult(errors=[e])
+
+        operation_ast = get_operation_ast(document, operation_name)
+        operation_type = operation_ast.operation.value
+
         if sentry_sdk:
             if sentry_sdk.Hub.current.scope.transaction:
-                try:
-                    document = parse(query)
-                except Exception as e:
-                    return ExecutionResult(errors=[e])
-
-                operation_ast = get_operation_ast(document, operation_name)
-                operation_type = operation_ast.operation.value
-
                 if operation_name:
                     sentry_sdk.Hub.current.scope.transaction.name = operation_name
                 if operation_type:
                     sentry_sdk.Hub.current.scope.transaction.op = operation_type
 
-        return super().execute_graphql_request(
-            request, data, query, variables, operation_name, show_graphiql
-        )
+        with pghistory.context(graphql_operation_name=operation_name, graphql_operation_type=operation_type):
+            return super().execute_graphql_request(
+                request, data, query, variables, operation_name, show_graphiql
+            )
