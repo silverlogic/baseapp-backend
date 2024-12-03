01d35bdf672ee3dc23e99de70113d9649aa66284
Henrique Pozzolini <hp@tsl.io>
2024-12-03 17:43:46 -0300

    BA-1779-be-multiple-profiles-member-list

--- baseapp-profiles/baseapp_profiles/graphql/filters.py
+++ baseapp-profiles/baseapp_profiles/graphql/filters.py
@@ -1,8 +1,9 @@
 import django_filters
 import swapper
-from django.db.models import Q
+from django.db.models import Case, IntegerField, Q, Value, When
 
 Profile = swapper.load_model("baseapp_profiles", "Profile")
+ProfileUserRole = swapper.load_model("baseapp_profiles", "ProfileUserRole")
 
 
 class ProfileFilter(django_filters.FilterSet):
@@ -22,3 +23,35 @@ class ProfileFilter(django_filters.FilterSet):
 
     def filter_q(self, queryset, name, value):
         return queryset.filter(Q(name__icontains=value) | Q(url_paths__path__icontains=value))
+
+
+class MemberOrderingFilter(django_filters.OrderingFilter):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.extra["choices"] += [
+            ("status", "Status"),
+        ]
+
+    def filter(self, qs, value):
+        if value is None:
+            return qs
+
+        if any(v == "status" for v in value):
+            status_order = Case(
+                When(status=ProfileUserRole.ProfileRoleStatus.PENDING.value, then=Value(1)),
+                When(status=ProfileUserRole.ProfileRoleStatus.INACTIVE.value, then=Value(2)),
+                When(status=ProfileUserRole.ProfileRoleStatus.ACTIVE.value, then=Value(3)),
+                default=Value(4),
+                output_field=IntegerField(),
+            )
+            return qs.order_by(status_order)
+
+        return super().filter(qs, value)
+
+
+class MemberFilter(django_filters.FilterSet):
+    order_by = MemberOrderingFilter()
+
+    class Meta:
+        model = ProfileUserRole
+        fields = ["role", "order_by"]
