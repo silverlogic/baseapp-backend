80f6e7c2250d2d182cb526d5220e1234d3fc097b
Alisson Patricio <eu@alisson.net>
2025-01-21 16:42:11 -0300

    baseapp-reactions: move to baseapp-backend

--- baseapp/baseapp_reactions/apps.py
+++ baseapp/baseapp_reactions/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class ReactionsConfig(AppConfig):
     default = True
     name = "baseapp_reactions"
+    label = "baseapp_reactions"
     verbose_name = "BaseApp Reactions"
     default_auto_field = "django.db.models.AutoField"
 
--- baseapp/testproject/settings.py
+++ baseapp/testproject/settings.py
@@ -4,6 +4,8 @@ from django.utils.translation import gettext_lazy as _
 # Application definition
 INSTALLED_APPS += [
     "graphene_django",
+    "notifications",
+    "push_notifications",
     "baseapp_profiles",
     "baseapp_reactions",
     "baseapp_reports",
@@ -13,6 +15,7 @@ INSTALLED_APPS += [
     "baseapp_follows",
     "baseapp_blocks",
     "baseapp.activity_log",
+    "baseapp_notifications",
     "testproject.testapp",
     "testproject.comments",
     "testproject.profiles",
@@ -43,19 +46,23 @@ CELERY_TASK_EAGER_PROPAGATES = True
 
 AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
+    "baseapp_auth.permissions.UsersPermissionsBackend",
     "baseapp_profiles.permissions.ProfilesPermissionsBackend",
     "baseapp_comments.permissions.CommentsPermissionsBackend",
     "baseapp.activity_log.permissions.ActivityLogPermissionsBackend",
+    "baseapp_reactions.permissions.ReactionsPermissionsBackend",
 ]
 
 ADMIN_TIME_ZONE = "UTC"
 
 BASEAPP_COMMENTS_ENABLE_NOTIFICATIONS = False
-
-# NOTIFICATIONS_NOTIFICATION_MODEL = "baseapp_notifications.Notification"
+BASEAPP_REACTIONS_ENABLE_NOTIFICATIONS = False
 
 # Comments
 BASEAPP_COMMENTS_COMMENT_MODEL = "comments.Comment"
 
 # Profiles
 BASEAPP_PROFILES_PROFILE_MODEL = "profiles.Profile"
+
+# Notifications
+NOTIFICATIONS_NOTIFICATION_MODEL = "baseapp_notifications.Notification"
