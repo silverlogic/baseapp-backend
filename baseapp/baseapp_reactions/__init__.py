dcfa6b92cad7bd0f7fad4de969e812db61fd04b3
Alisson Patricio <eu@alisson.net>
2025-01-20 23:20:41 -0300

    add reactions fall back

---
+++ baseapp/baseapp_reactions/__init__.py
@@ -0,0 +1,8 @@
+import warnings
+from baseapp.reactions import *  # noqa
+
+warnings.warn(
+    "Importing from baseapp_reactions is deprecated. Use baseapp.reactions instead.",
+    DeprecationWarning,
+    stacklevel=2
+)
