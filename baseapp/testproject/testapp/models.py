da1f1f69bca1e18fbf1fbcee6296a610ce30d977
Alisson Patricio <eu@alisson.net>
2025-01-29 23:16:16 -0300

    baseapp-ratings: move baseapp-backend

--- baseapp/testproject/testapp/models.py
+++ baseapp/testproject/testapp/models.py
@@ -1,12 +1,15 @@
 from baseapp_auth.models import AbstractUser
+from baseapp_core.graphql.models import RelayModel
 from baseapp_profiles.models import ProfilableModel
 from baseapp_profiles.signals import update_user_profile
 from django.db.models.signals import post_save
 from django.utils import timezone
 from model_utils import FieldTracker
 
+from baseapp_ratings.models import RatableModel
 
-class User(AbstractUser, ProfilableModel):
+
+class User(AbstractUser, RelayModel, RatableModel, ProfilableModel):
     # FieldTracker doesn't work with abstract model classes
     tracker = FieldTracker(fields=["is_superuser", "password"])
 
