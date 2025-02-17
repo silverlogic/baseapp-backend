b614d4dcab7bdc1e39eb8917787c4bbad164e56b
vitorguima <vitorguima@gmail.com>
2025-02-17 18:11:23 -0300

    BA2257-graphql-query-optimize

--- baseapp-auth/baseapp_auth/tests/graphql/test_queries_user.py
+++ baseapp-auth/baseapp_auth/tests/graphql/test_queries_user.py
@@ -15,6 +15,58 @@ QUERY = """
     }
 """
 
+MALICIOUS_QUERY = """
+    query MaliciousLoop {
+        users {
+            edges {
+                node {
+                    comments {
+                        edges {
+                            node {
+                                user {
+                                    comments {
+                                        edges {
+                                            node {
+                                                user {
+                                                    comments {
+                                                        edges {
+                                                            node {
+                                                                id
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+"""
+
+QUERY_USERS_LIST = """
+    query GetUsersList {
+        users {
+            edges {
+                node {
+                    id
+                    firstName
+                    lastName
+                    profile {
+                        id
+                        name
+                    }
+                }
+            }
+        }
+    }
+"""
+
 
 def test_anon_can_query_user_by_pk(graphql_client):
     user = UserFactory()
@@ -78,3 +130,28 @@ def test_user_with_perm_can_view_others_private_field(django_user_client, graphq
     content = response.json()
 
     assert content["data"]["user"]["email"] == user.email
+
+
+def test_overcomplex_queries_are_not_executed(graphql_client_with_queries):
+    response, queries = graphql_client_with_queries(MALICIOUS_QUERY)
+    content = response.json()
+
+    assert content["errors"][0]["message"] == "Query complexity exceeds the maximum allowed of 3"
+    assert queries.count == 0
+
+
+def test_anon_can_query_users_list_with_optimized_query(graphql_client_with_queries):
+    users = UserFactory.create_batch(10)
+    response, queries = graphql_client_with_queries(QUERY_USERS_LIST)
+    content = response.json()
+
+    assert queries.count == 3
+    for i, user in enumerate(users):
+        assert content["data"]["users"]["edges"][i]["node"]["id"] == str(user.relay_id)
+        assert content["data"]["users"]["edges"][i]["node"]["firstName"] == user.first_name
+        assert content["data"]["users"]["edges"][i]["node"]["lastName"] == user.last_name
+
+    # With optimizer queries are expected to be 3 and retrieving just the queried fields:
+    # SELECT "users_user"."id", "users_user"."profile_id", "users_user"."first_name", "users_user"."last_name", ("users_user"."password_changed_date" + (730 days, 0:00:00)::interval) AS "password_expiry_date", (("users_user"."password_changed_date" + (730 days, 0:00:00)::interval) AT TIME ZONE UTC)::date <= 2025-02-28 AS "is_password_expired", "profiles_profile"."id", "profiles_profile"."name" FROM "users_user" LEFT OUTER JOIN "profiles_profile" ON ("users_user"."profile_id" = "profiles_profile"."id") WHERE "users_user"."is_active"
+    # SELECT COUNT(*) AS "__count" FROM "users_user" WHERE "users_user"."is_active"
+    # SELECT "users_user"."id", "users_user"."profile_id", "users_user"."first_name", "users_user"."last_name", ("users_user"."password_changed_date" + (730 days, 0:00:00)::interval) AS "password_expiry_date", (("users_user"."password_changed_date" + (730 days, 0:00:00)::interval) AT TIME ZONE UTC)::date <= 2025-02-28 AS "is_password_expired", "profiles_profile"."id", "profiles_profile"."name" FROM "users_user" LEFT OUTER JOIN "profiles_profile" ON ("users_user"."profile_id" = "profiles_profile"."id") WHERE "users_user"."is_active" LIMIT 10
