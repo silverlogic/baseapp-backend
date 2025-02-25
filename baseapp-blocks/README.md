8e29e08af5a1087506b1c6481b5737bd9ac85d65
Alisson Patricio <eu@alisson.net>
2025-02-25 10:45:01 -0300

    delete migrated packages folders

--- baseapp-blocks/README.md
+++
@@ -1,3 +0,0 @@
-# BaseApp Blocks
-
-This package has been moved to [baseapp-packend](https://pypi.org/project/baseapp-backend/) package.
--- baseapp-cloudflare-stream-field/testproject/testapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-from baseapp_cloudflare_stream_field import CloudflareStreamField
-
-
-class Post(models.Model):
-    title = models.CharField(max_length=100)
-    video = CloudflareStreamField(null=True, blank=True, downloadable=False)
