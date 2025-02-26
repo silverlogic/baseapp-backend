62a96c16cf4747777773d6157747e4cfce94ef24
Alisson Patricio <eu@alisson.net>
2025-02-26 16:48:35 -0300

    baseapp_pages: fix tests and permissions
    by using app_label instead of hardcoded baseapp_pages

--- baseapp/baseapp_pages/permissions.py
+++ baseapp/baseapp_pages/permissions.py
@@ -2,11 +2,12 @@ import swapper
 from django.contrib.auth.backends import BaseBackend
 
 Page = swapper.load_model("baseapp_pages", "Page")
+page_app_label = Page._meta.app_label
 
 
 class PagesPermissionsBackend(BaseBackend):
     def has_perm(self, user_obj, perm, obj=None):
-        if perm == "baseapp_pages.view_page":
+        if perm == f"{page_app_label}.view_page":
             if not obj:
                 # Anyone can view a page
                 return True
@@ -15,9 +16,9 @@ class PagesPermissionsBackend(BaseBackend):
                     return True
                 else:
                     # Only users who has change permission can view unpublished pages
-                    return user_obj.has_perm("baseapp_pages.change_page", obj)
+                    return user_obj.has_perm(f"{page_app_label}.change_page", obj)
 
-        if perm in ["baseapp_pages.change_page", "baseapp_pages.delete_page"]:
+        if perm in [f"{page_app_label}.change_page", f"{page_app_label}.delete_page"]:
             if user_obj.is_authenticated and isinstance(obj, Page):
                 # Owner can change and delete their own pages
                 if obj.user_id == user_obj.id:
