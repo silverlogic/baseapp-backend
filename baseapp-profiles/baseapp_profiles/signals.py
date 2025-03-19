92a031509c91220b96ffe97a3726355b18895c83
Nwuguru Sunday <sundayguru@gmail.com>
2025-03-19 19:11:57 +0100

    baseapp-auth: BA-2352 allow users to register using first/last name (#264)
    * BA-2352: Remove name field from profile and remove signals
    
    * BA-2352: Add a method to update or create profile
    
    * BA-2352: Support first and last name in register endpoint
    
    * BA-2352: Remove migrations
    
    * BA-2352: Add name translation
    
    * BA-2352: Add name to organization abstract model
    
    * BA-2352: Fix lints and migrations
    
    * BA-2352: Fix lint and tests
    
    * BA-2352: Increase version

--- baseapp-profiles/baseapp_profiles/signals.py
+++ baseapp-profiles/baseapp_profiles/signals.py
@@ -1,6 +1,4 @@
-import swapper
-from django.contrib.contenttypes.models import ContentType
-from django.core.exceptions import ObjectDoesNotExist
+from .models import update_or_create_profile
 
 
 def update_user_profile(instance, created, **kwargs):
@@ -9,36 +7,4 @@ def update_user_profile(instance, created, **kwargs):
     to automatically create a user profile when an instance of User is saved for the first time
     """
 
-    if created and not instance.profile_id:
-        Profile = swapper.load_model("baseapp_profiles", "Profile")
-        target_content_type = ContentType.objects.get_for_model(instance)
-
-        name = instance.get_full_name()
-
-        profile = Profile.objects.create(
-            name=name,
-            owner=instance,
-            target_content_type=target_content_type,
-            target_object_id=instance.pk,
-        )
-
-        instance.profile = profile
-        instance.save(update_fields=["profile"])
-
-
-def update_user_name(instance, created, **kwargs):
-    """
-    Call django.db.models.signals.post_save.connect(update_user_name, sender=Profile)
-    to update the first_name of the user object associated to a Profile instance
-    whenever the profile name is updated.
-    """
-
-    try:
-        user = instance.user
-    except ObjectDoesNotExist:
-        return
-
-    if user.first_name != instance.name:
-        user.first_name = instance.name
-        user.last_name = ""
-        user.save(update_fields=["first_name", "last_name"])
+    update_or_create_profile(instance, instance, f"{instance.first_name} {instance.last_name}")
