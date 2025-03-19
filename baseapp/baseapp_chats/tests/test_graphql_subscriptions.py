29538a84ff5069597ff2672c86093efa9ce6ce15
tsl-ps2 <ps2@tsl.io>
2025-03-19 19:01:39 +0100

    baseapp-chats [BA-2236]: Change message subscription to not publish to sender (#245)
    * BA-2236
    
    * BA-2236 Check user permission

--- baseapp/baseapp_chats/tests/test_graphql_subscriptions.py
+++ baseapp/baseapp_chats/tests/test_graphql_subscriptions.py
@@ -35,8 +35,8 @@ async def test_user_recieves_news_message_subscription_event(
         payload={
             "query": textwrap.dedent(
                 """
-                subscription op_name($roomId: ID!) {
-                  chatRoomOnMessage(roomId: $roomId) {
+                subscription op_name($roomId: ID!, $profileId: ID!) {
+                  chatRoomOnMessage(roomId: $roomId, profileId: $profileId) {
                     message {
                       node {
                         id
@@ -47,7 +47,10 @@ async def test_user_recieves_news_message_subscription_event(
                 }
                 """
             ),
-            "variables": {"roomId": room.relay_id},
+            "variables": {
+                "roomId": room.relay_id,
+                "profileId": django_user_client.user.profile.relay_id,
+            },
             "operationName": "op_name",
         },
     )
@@ -93,8 +96,8 @@ async def test_build_absolute_uri_on_graphql_subscription(
         payload={
             "query": textwrap.dedent(
                 """
-                subscription op_name($roomId: ID!) {
-                  chatRoomOnMessage(roomId: $roomId) {
+                subscription op_name($roomId: ID!, $profileId: ID!) {
+                  chatRoomOnMessage(roomId: $roomId, profileId: $profileId) {
                     message {
                       node {
                         profile {
@@ -108,7 +111,10 @@ async def test_build_absolute_uri_on_graphql_subscription(
                 }
                 """
             ),
-            "variables": {"roomId": room.relay_id},
+            "variables": {
+                "roomId": room.relay_id,
+                "profileId": django_user_client.user.profile.relay_id,
+            },
             "operationName": "op_name",
         },
     )
