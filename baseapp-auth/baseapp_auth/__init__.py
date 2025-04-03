53044785372ed1358ec1625320b312a964e912aa
Alisson Patricio <eu@alisson.net>
2025-04-03 16:20:29 -0300

    baseapp-auth: deprecate pypi package

---
+++ baseapp-auth/baseapp_auth/__init__.py
@@ -0,0 +1,7 @@
+import warnings
+
+warnings.warn(
+    "This package has been deprecated in favor of baseapp-backend. Please update your dependencies.",
+    DeprecationWarning,
+    stacklevel=2,
+)
