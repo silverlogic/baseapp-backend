bb544ff85e95c3c35067aec5389f0ee6fbbc4c86
Lúcio BJ <lb@tsl.io>
2025-04-23 16:53:19 -0300

    None or empty target object id

--- baseapp_reports/graphql/queries.py
+++ baseapp_reports/graphql/queries.py
@@ -16,8 +16,8 @@ class ReportsQueries:
     all_report_types = DjangoFilterConnectionField(ReportTypeObjectType)
 
     def resolve_all_report_types(self, info, **kwargs):
-        target_object_id = kwargs.get("target_object_id", None)
-        if not target_object_id:
+        target_object_id = kwargs.get("target_object_id")
+        if target_object_id is None or target_object_id == "":
             return ReportType.objects.all()
         obj = get_obj_from_relay_id(info, target_object_id)
         content_type = ContentType.objects.get_for_model(obj)
--- baseapp_reports/tests/test_graphql_queries.py
+++ baseapp_reports/tests/test_graphql_queries.py
@@ -25,23 +25,19 @@ query reportTypesQuery($topLevelOnly: Boolean, $targetObjectId: String) {
 
 def test_anon_can_list_report_types(graphql_client):
     profile = ProfileFactory()
-
     variables = {
         "targetObjectId": profile.relay_id,
         "topLevelOnly": True,
     }
-
     response = graphql_client(REPORT_TYPES_LIST_GRAPHQL, variables=variables)
     content = response.json()
-
     assert "errors" not in content
     assert len(content["data"]["allReportTypes"]["edges"]) > 0
 
 
 def test_user_can_get_report_types(django_user_client, graphql_user_client):
-    response = graphql_user_client(REPORT_TYPES_LIST_GRAPHQL, variables={})
+    response = graphql_user_client(REPORT_TYPES_LIST_GRAPHQL)
     content = response.json()
-
     assert (
         len(content["data"]["allReportTypes"]["edges"]) == 11
     )  # Created by baseapp_reports migrations
@@ -55,7 +51,6 @@ def test_user_can_get_report_type_filtered_by_top_level(django_user_client, grap
 
 def test_user_can_get_report_type_filtered_by_content_type(django_user_client, graphql_user_client):
     other_profile = ProfileFactory()
-
     response = graphql_user_client(
         REPORT_TYPES_LIST_GRAPHQL, variables={"targetObjectId": other_profile.relay_id}
     )
