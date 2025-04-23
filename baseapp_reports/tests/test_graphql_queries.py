1b9f80c9d784b04aa90b2f9157dc0b3365846a82
Lúcio BJ <lb@tsl.io>
2025-04-23 17:30:22 -0300

    Update tests?

--- baseapp_reports/tests/test_graphql_queries.py
+++ baseapp_reports/tests/test_graphql_queries.py
@@ -24,34 +24,21 @@ query reportTypesQuery($topLevelOnly: Boolean, $targetObjectId: String) {
 
 
 def test_anon_can_list_report_types(graphql_client):
-    profile = ProfileFactory()
-    variables = {
-        "targetObjectId": profile.relay_id,
-        "topLevelOnly": True,
-    }
-    response = graphql_client(REPORT_TYPES_LIST_GRAPHQL, variables=variables)
+    response = graphql_client(REPORT_TYPES_LIST_GRAPHQL)
     content = response.json()
     assert "errors" not in content
     assert len(content["data"]["allReportTypes"]["edges"]) > 0
 
 
-def test_user_can_get_report_types(django_user_client, graphql_user_client):
-    response = graphql_user_client(REPORT_TYPES_LIST_GRAPHQL)
-    content = response.json()
-    assert (
-        len(content["data"]["allReportTypes"]["edges"]) == 11
-    )  # Created by baseapp_reports migrations
-
-
-def test_user_can_get_report_type_filtered_by_top_level(django_user_client, graphql_user_client):
-    response = graphql_user_client(REPORT_TYPES_LIST_GRAPHQL, variables={"topLevelOnly": True})
+def test_anon_can_get_report_type_filtered_by_top_level(graphql_client):
+    response = graphql_client(REPORT_TYPES_LIST_GRAPHQL, variables={"topLevelOnly": True})
     content = response.json()
     assert len(content["data"]["allReportTypes"]["edges"]) == 8  # 11 total, with 3 subtypes
 
 
-def test_user_can_get_report_type_filtered_by_content_type(django_user_client, graphql_user_client):
+def test_anon_can_get_report_type_filtered_by_content_type(graphql_client):
     other_profile = ProfileFactory()
-    response = graphql_user_client(
+    response = graphql_client(
         REPORT_TYPES_LIST_GRAPHQL, variables={"targetObjectId": other_profile.relay_id}
     )
     content = response.json()
