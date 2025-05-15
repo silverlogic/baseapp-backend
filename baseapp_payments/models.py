0d9617e9d46bbe10d94070cd28f488caba2ae9e7
Lúcio BJ <lb@tsl.io>
2025-05-15 15:18:32 -0300

    Change to timestamped model

--- baseapp_payments/models.py
+++ baseapp_payments/models.py
@@ -2,10 +2,11 @@ import swapper
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
-from model_utils.fields import FieldTracker
+from model_utils import FieldTracker
+from model_utils.models import TimeStampedModel
 
 
-class BaseCustomer(models.Model):
+class BaseCustomer(TimeStampedModel):
     entity_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     entity_id = models.PositiveIntegerField()
     entity = GenericForeignKey("entity_type", "entity_id")
@@ -20,15 +21,14 @@ class BaseCustomer(models.Model):
     tracker = FieldTracker(fields=["entity"])
 
     def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
         if self.tracker.has_changed("entity"):
             if self.entity:
                 self.entity_type = ContentType.objects.get_for_model(self.entity)
                 self.entity_id = self.entity.id
-                self.save()
+        super().save(*args, **kwargs)
 
 
-class BaseSubscription(models.Model):
+class BaseSubscription(TimeStampedModel):
     remote_customer_id = models.CharField(max_length=255)
     remote_subscription_id = models.CharField(max_length=255)
 
