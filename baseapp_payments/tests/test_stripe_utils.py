9d11e5f951dda3d61a5f7462db5bb23f7dc5347e
Lúcio BJ <lb@tsl.io>
2025-05-20 14:06:10 -0300

    Fix utils test

--- baseapp_payments/tests/test_stripe_utils.py
+++ baseapp_payments/tests/test_stripe_utils.py
@@ -14,7 +14,7 @@ def test_check_customer_id_for_user_success():
     with patch("baseapp_payments.utils.StripeService.retrieve_customer") as mock_retrieve_customer:
         mock_retrieve_customer.return_value = mock_customer
         user = UserFactory()
-        CustomerFactory(entity=user, remote_customer_id="cus_123")
+        CustomerFactory(entity=user.profile, remote_customer_id="cus_123")
         stripe_service = StripeService()
         result = stripe_service.checkCustomerIdForUser("cus_123", user)
         assert result is True
@@ -25,7 +25,7 @@ def test_check_customer_id_for_user_customer_not_found():
     with patch("baseapp_payments.utils.StripeService.retrieve_customer") as mock_retrieve_customer:
         mock_retrieve_customer.return_value = None
         user = UserFactory()
-        CustomerFactory(entity=user, remote_customer_id="cus_123")
+        CustomerFactory(entity=user.profile, remote_customer_id="cus_123")
         stripe_service = StripeService()
         result = stripe_service.checkCustomerIdForUser("non_existent_cus_123", user)
         assert result is False
@@ -36,7 +36,7 @@ def test_retrieve_customer_by_email():
     with patch("baseapp_payments.utils.StripeService.retrieve_customer") as mock_retrieve_customer:
         mock_retrieve_customer.return_value = mock_customer
         user = UserFactory()
-        CustomerFactory(entity=user, remote_customer_id="cus_123")
+        CustomerFactory(entity=user.profile, remote_customer_id="cus_123")
         stripe_service = StripeService()
         result = stripe_service.retrieve_customer(email=user.email)
         assert result == mock_customer
@@ -47,7 +47,7 @@ def test_retrieve_customer_by_customer_id():
     with patch("baseapp_payments.utils.StripeService.retrieve_customer") as mock_retrieve_customer:
         mock_retrieve_customer.return_value = mock_customer
         user = UserFactory()
-        CustomerFactory(entity=user, remote_customer_id="cus_123")
+        CustomerFactory(entity=user.profile, remote_customer_id="cus_123")
         stripe_service = StripeService()
         result = stripe_service.retrieve_customer(customer_id="cus_123")
         assert result == mock_customer
