31965629333f007c42c0e7bb2d6d681c6174f0a4
vitorguima <vitorguima@gmail.com>
2025-05-26 16:37:29 -0300

    fix: lint 2

--- baseapp_core/graphql/consumers.py
+++ baseapp_core/graphql/consumers.py
@@ -3,18 +3,15 @@ import contextvars
 import functools
 import logging
 import sys
-import swapper
 
 import channels_graphql_ws
+import swapper
 from channels.db import database_sync_to_async
 from django.contrib.auth.models import AnonymousUser
 from graphene_django.settings import graphene_settings
 from rest_framework.authtoken.models import Token
 
-from baseapp_core.graphql import (
-    get_pk_from_relay_id,
-)
-
+from baseapp_core.graphql import get_pk_from_relay_id
 
 python_version = sys.version_info
 
