719ea51d676b87ddbfab84e7a5bd8c728af7f4ed
Lúcio BJ <lb@tsl.com>
2025-06-26 17:15:21 -0300

    Lint

--- baseapp_payments/tests/test_permissions.py
+++ baseapp_payments/tests/test_permissions.py
@@ -11,15 +11,19 @@ def test_has_self_customer_permissions_failure():
     CustomerFactory(entity=user.profile, remote_customer_id="cus_123")
     assert not HasCustomerPermissions().has_permission(user, "view_customer")
 
+
 @pytest.mark.django_db
 def test_has_self_customer_permissions_success():
     user = UserFactory()
     CustomerFactory(entity=user.profile, remote_customer_id="cus_123", authorized_users=[user])
     assert HasCustomerPermissions().has_permission(user, "view_customer")
 
+
 @pytest.mark.django_db
 def test_has_other_entity_customer_permissions_success():
     user = UserFactory()
     other_user = UserFactory()
-    CustomerFactory(entity=other_user.profile, remote_customer_id="cus_123", authorized_users=[user])
+    CustomerFactory(
+        entity=other_user.profile, remote_customer_id="cus_123", authorized_users=[user]
+    )
     assert HasCustomerPermissions().has_permission(user, "view_customer")
