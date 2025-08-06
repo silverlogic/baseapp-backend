dd4b3610b8ce5e589b33875dc35a18f9a29705ea
Lúcio BJ <lb@tsl.com>
2025-08-06 17:24:39 -0300

    Lint

--- baseapp_payments/views.py
+++ baseapp_payments/views.py
@@ -10,7 +10,6 @@ from rest_framework.response import Response
 from baseapp_core.graphql import get_pk_from_relay_id
 from baseapp_core.rest_framework.decorators import action
 from baseapp_core.rest_framework.mixins import DestroyModelMixin
-from baseapp_core.graphql import get_pk_from_relay_id
 
 from .models import Subscription
 from .permissions import DRFCustomerPermissions, DRFSubscriptionPermissions
