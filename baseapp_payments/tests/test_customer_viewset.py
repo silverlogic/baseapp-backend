90fefd584e3f0a16d15d960b1d2f5441ec8f20a3
Lúcio BJ <lb@tsl.com>
2025-08-13 10:35:48 -0300

    Test

--- baseapp_payments/tests/test_customer_viewset.py
+++ baseapp_payments/tests/test_customer_viewset.py
@@ -6,6 +6,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.urls import reverse
 from rest_framework import status
 
+from baseapp_core.utils import get_relay_id_from_pk
 from baseapp_core.tests.helpers import responseEquals
 from baseapp_payments.tests.factories import CustomerFactory
 
@@ -39,9 +40,10 @@ class TestCustomerCreateView:
     @patch("baseapp_payments.views.StripeService.create_customer")
     def test_user_can_create_customer(self, mock_create_customer, user_client):
         mock_create_customer.return_value = {"id": "cus_123"}
+        relay_id = get_relay_id_from_pk(user_client.user.profile.id)
         response = user_client.post(
             reverse(self.viewname),
-            data={"entity_id": user_client.user.profile.id},
+            data={"entity_id": relay_id},
         )
         responseEquals(response, status.HTTP_201_CREATED)
         assert Customer.objects.all().count() == 1
