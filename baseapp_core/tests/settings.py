4fd82436bc164a09dfa15d04cb484cc1026e725f
vitorguima <vitorguima@gmail.com>
2025-11-25 12:11:38 -0300

    hotfix/AttributeError: Page object has no attribute title_en_us. Did you mean: title_en

--- baseapp_core/tests/settings.py
+++ baseapp_core/tests/settings.py
@@ -144,7 +144,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "en"
 LANGUAGES = [("en", _("English"))]
 
 TIME_ZONE = "UTC"
