e8fdcad2ab852fb2c2068a7d5418b4ad49bdc5b5
Filipe de Castro <fc@tsl.io>
2026-01-05 14:49:46 +0000

    BA-2959 migrate admin to allauth (#361)
    demo: https://www.loom.com/share/dfd586a8c9104c918c1ca784fcb42c53

---
+++ baseapp_auth/allauth/urls.py
@@ -0,0 +1,31 @@
+"""
+URL patterns for django-allauth admin redirects.
+
+These URLs redirect Django admin authentication URLs to django-allauth views,
+providing a seamless integration between Django admin and allauth.
+"""
+
+from django.urls import include, path, re_path
+from django.views.generic.base import RedirectView
+
+urlpatterns = [
+    re_path(
+        r"^admin/login/",
+        RedirectView.as_view(pattern_name="account_login", query_string=True, permanent=False),
+    ),
+    re_path(
+        r"^admin/logout/",
+        RedirectView.as_view(pattern_name="account_logout", query_string=True, permanent=True),
+    ),
+    re_path(
+        r"^admin/password_change/",
+        RedirectView.as_view(
+            pattern_name="account_change_password", query_string=True, permanent=True
+        ),
+    ),
+    re_path(
+        r"^admin/password_change/done/",
+        RedirectView.as_view(pattern_name="admin:index", query_string=True, permanent=True),
+    ),
+    path("accounts/", include("allauth.urls")),
+]
