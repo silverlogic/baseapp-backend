32617e4eb1469e4b38e386ef41c016adf6c17dd5
Alexandre Anício <aa@tsl.io>
2026-03-17 12:31:55 -0300

    chore: running black in the codebase

--- baseapp_core/models.py
+++ baseapp_core/models.py
@@ -184,8 +184,7 @@ def insert_document_id_trigger():
         level=pgtrigger.Row,
         when=pgtrigger.After,
         operation=pgtrigger.Insert,
-        func=DocumentIdFunc(
-            """
+        func=DocumentIdFunc("""
             INSERT INTO {document_id_table} (public_id, content_type_id, object_id, created, modified)
             VALUES (
                 gen_random_uuid(),
@@ -196,8 +195,7 @@ def insert_document_id_trigger():
             )
             ON CONFLICT (content_type_id, object_id) DO NOTHING;
             RETURN NULL;
-            """
-        ),
+            """),
     )
 
 
@@ -210,15 +208,13 @@ def delete_document_id_trigger():
         level=pgtrigger.Row,
         when=pgtrigger.After,
         operation=pgtrigger.Delete,
-        func=DocumentIdFunc(
-            """
+        func=DocumentIdFunc("""
             DELETE FROM {document_id_table}
             WHERE
                 content_type_id = (SELECT id FROM {content_type_table} WHERE app_label = '{app_label}' AND model = '{model_name}')
                 AND object_id = OLD.{pk};
             RETURN NULL;
-            """
-        ),
+            """),
     )
 
 
