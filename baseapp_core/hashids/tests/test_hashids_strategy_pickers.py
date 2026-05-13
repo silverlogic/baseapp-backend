49afa8c90ae11f567cc2d8235f927ab9d710b396
Alisson Patricio <eu@alisson.net>
2026-05-13 15:28:48 +0000

    undo test_hashids_strategy_pickers.py

--- baseapp_core/hashids/tests/test_hashids_strategy_pickers.py
+++ baseapp_core/hashids/tests/test_hashids_strategy_pickers.py
@@ -268,10 +268,14 @@ class TestGraphQLGetNodeFromGlobalIdUsingStrategy:
         mock_info = MagicMock()
         mock_only_type = MagicMock()
 
-        with pytest.raises(Exception):
+        with pytest.raises(Exception) as e:
             graphql_get_node_from_global_id_using_strategy(
                 mock_info, str(dummy_instance.pk), mock_only_type
             )
+        assert (
+            f"{dummy_instance.__class__.__name__} is not compatible with the PK strategy"
+            in str(e.value)
+        )
 
     def test_users_legacy_with_pk_model_with_pk_strategy(self):
         dummy_instance = DummyLegacyWithPkModelFactory()
