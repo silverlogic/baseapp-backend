202f249d5c204947d0f9da254a85dfe117ddeabd
Alisson Patricio <eu@alisson.net>
2026-07-18 23:37:00 +0000

    feat(geo): add GeoJSONFeatureForm with Point|Polygon validation

--- baseapp_geo/graphql/mutations.py
+++ baseapp_geo/graphql/mutations.py
@@ -1,2 +1,26 @@
+import swapper
+from django import forms
+from django.contrib.gis.geos import GEOSGeometry
+from django.utils.translation import gettext_lazy as _
+
+from ..models import ALLOWED_GEOMETRY_TYPES
+
+GeoJSONFeature = swapper.load_model("baseapp_geo", "GeoJSONFeature")
+app_label = GeoJSONFeature._meta.app_label
+ObjectType = GeoJSONFeature.get_graphql_object_type()
+
+
+class GeoJSONFeatureForm(forms.ModelForm):
+    class Meta:
+        model = GeoJSONFeature
+        fields = ("name", "description", "feature_type", "geometry")
+
+    def clean_geometry(self) -> GEOSGeometry:
+        geometry = self.cleaned_data.get("geometry")
+        if geometry and geometry.geom_type not in ALLOWED_GEOMETRY_TYPES:
+            raise forms.ValidationError(_("Geometry must be a Point or a Polygon."))
+        return geometry
+
+
 class GeoMutations:
     pass
